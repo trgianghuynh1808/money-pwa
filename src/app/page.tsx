6529640446d8c1773999208b98a38eb0c359b93b
Giang Huynh <trgianghuynhdev1808@gmail.com>
2024-01-18 18:40:56 +0700

    feat: prevent pull-refresh action on chrome & safari

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find in-depth information aut Next.js features and API.
           </p>
         </a>
 
