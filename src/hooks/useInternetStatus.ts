c44cbddc7f561de0daf928d57db40b94fadc5e6e
Giang Huynh <trgianghuynhdev1808@gmail.com>
2024-02-09 19:59:42 +0700

    fix: check internetConnection

--- src/hooks/useInternetStatus.ts
+++ src/hooks/useInternetStatus.ts
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react'
 
 export function useInternetStatus() {
-  const [isOnline, setIsOnline] = useState<boolean>(window?.navigator?.onLine)
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+  const [isOnline, setIsOnline] = useState<boolean>(
+    typeof window !== 'undefined' ? window.navigator.onLine : false,
+  )
 
   useEffect(() => {
+    const handleChangeStatus = () => {
+      setIsOnline(navigator.onLine)
+    }
+
     window.addEventListener('offline', () => {
       setIsOnline(false)
     })
@@ -13,10 +20,18 @@ export function useInternetStatus() {
 
     // *INFO: clean up to avoid memory-leak
     return () => {
-      window.removeEventListener('online', () => {})
-      window.removeEventListener('offline', () => {})
+      window.removeEventListener('online', handleChangeStatus)
+      window.removeEventListener('offline', handleChangeStatus)
     }
   }, [])
+
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true)
+    } else {
+      setIsOnline(navigator.onLine)
+    }
+  }, [isMounted])
 
   return { isOnline }
 }
