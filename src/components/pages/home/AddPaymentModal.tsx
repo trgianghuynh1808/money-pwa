44c9ff7f02c498ce1812475fdb20092ef534728b
Giang Huynh <trgianghuynhdev1808@gmail.com>
2024-02-10 23:50:50 +0700

    fix: try to tel input

--- src/components/pages/home/AddPaymentModal.tsx
+++ src/components/pages/home/AddPaymentModal.tsx
@@ -58,7 +58,7 @@ export default function AddPaymentModal(
                     <input
                       className="mt-2 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                       id="inline-full-name"
-                      type="text"
+                      type="tel"
                       placeholder="Ghi vô nào"
                       autoFocus
                       value={price}
