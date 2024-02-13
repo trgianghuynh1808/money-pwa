49c652295432cb64af2d2d67147d8f00f3fa1b22
Giang Huynh <trgianghuynhdev1808@gmail.com>
2024-02-13 17:48:58 +0700

    feat: implement Date pciker

--- src/components/pages/home/PaymentForm.tsx
+++ src/components/pages/home/PaymentForm.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+import DatePicker, { DateValueType } from 'react-tailwindcss-datepicker'
+import dayjs from 'dayjs'
 
 // *INFO: internal modules
 import CategoryLightBox from './CategoryLightBox'
@@ -12,14 +14,24 @@ export default function PaymentForm() {
     CATEGORY_OPTIONS[0],
   )
   const [price, setPrice] = useState<string>('')
+  const [pickDate, setPickDate] = useState<DateValueType>({
+    startDate: new Date(),
+    endDate: new Date(),
+  })
+  const isValidDate = useMemo(() => {
+    return Boolean(pickDate?.startDate) && Boolean(pickDate?.endDate)
+  }, [pickDate])
 
   function onClickAddBtn(): void {
     setIsOpenAddPaymentModal(true)
   }
 
   function handleSubmitForm(): void {
-    console.log({ price })
     setPrice('')
+  }
+
+  function onChangePicker(newValue: DateValueType): void {
+    setPickDate(newValue)
   }
 
   return (
@@ -32,16 +44,31 @@ export default function PaymentForm() {
         handleOnSubmit={handleSubmitForm}
       />
 
-      <div className="col-span-3">
+      <div className="col-span-4">
+        <DatePicker
+          primaryColor="purple"
+          containerClassName="relative w-full cursor-default rounded-lg shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+          useRange={false}
+          asSingle={true}
+          displayFormat={'DD/MM/YYYY'}
+          minDate={dayjs().startOf('month').toDate()}
+          maxDate={dayjs().toDate()}
+          value={pickDate}
+          onChange={onChangePicker}
+        />
+      </div>
+
+      <div className="col-span-2">
         <CategoryLightBox
           options={CATEGORY_OPTIONS}
           selectedOption={paymentCategoryOption}
           setSelectedOption={setPaymentCategoryOption}
         />
       </div>
-      <div className="col-span-1">
+      <div className="col-span-2">
         <button
-          className="inline-flex items-center justify-center w-full h-full text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500"
+          className="inline-flex items-center justify-center w-full h-10 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500 disabled:opacity-30"
+          disabled={!isValidDate}
           onClick={onClickAddBtn}
         >
           <span className="relative">Thêm</span>
